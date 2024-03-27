06ecf023e45203ce5f476c688c91eb2d9ba2f2f0
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-03-27 14:08:34 +0100

    Create start_cam.sh

---
+++ start_cam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Virtuelle Umgebung aktivieren
+source /home/pi/RPI_Cam/cam_venv/bin/activate
+
+# Verzeichnis wechseln
+cd /home/pi/RPI_Cam
+
+# Python Skript ausführen
+python rpi_basler_2.py
