7c686df48b2456ca48f1402f20f6b4a8fb9e691a
Fabian Pingel <96516664+fabianpingel@users.noreply.github.com>
2024-09-19 12:17:16 +0200

    Update install_sentinel.sh

--- install_sentinel.sh
+++ install_sentinel.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+# Überprüfen, ob das Skript auf einer ARM64-Architektur ausgeführt wird
+ARCH=$(uname -m)
+if [ "$ARCH" != "aarch64" ]; then
+    echo "Dieses Skript ist nur für ARM64 (aarch64) Architektur geeignet."
+    exit 1
+fi
+
 # Temporäres Verzeichnis setzen
 cd /tmp || exit
 
 # SentinelOne Agent herunterladen
-echo "Lade SentinelOne Agent herunter..."
+echo "Lade SentinelOne Agent für Nvidia Jetson herunter..."
 wget --no-check-certificate https://lspc-logging.linamar.com:8037/SentinelAgent-aarch64_linux_aarch64_v24_2_1_8.deb
 
 # Installation des SentinelOne Agents
 echo "Installiere SentinelOne Agent..."
 sudo dpkg -i ./SentinelAgent-aarch64_linux_aarch64_v24_2_1_8.deb
+
+# Überprüfen, ob die Installation erfolgreich war
+if [ $? -ne 0 ]; then
+    echo "Fehler bei der Installation des SentinelOne Agents."
+    exit 1
+fi
 
 # Management-Token setzen
 echo "Setze Management-Token..."
@@ -24,4 +37,4 @@ echo "Überprüfe den Status des SentinelOne Dienstes..."
 sudo /opt/sentinelone/bin/sentinelctl control status
 
 # Fertig
-echo "Installation abgeschlossen."
+echo "Installation auf Nvidia Jetson abgeschlossen."
